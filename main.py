4db391c9d651d12548f220f656f6a830cf25d2bf
CodeWithGamez <codewithgamez@gmail.com>
2020-08-23 17:44:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#Write code for using STRING CONCATENATION
+
+
+
+#1 Print a sentence using string concatenation
+
+
+
+
+
+
+
+#2 Print a sentence using string concatenation with 2 variables that are strings
+
+
+
+
+
+
+
+
+#3 Print a sentence using string concatenation with 3 variables that are 2 strings and one interger
+
+
+
